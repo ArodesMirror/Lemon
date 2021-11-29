d2558bcd3d831abd1b90fee8f89370ae86f6d89c
Arodes <2326785563@qq.com>
2021-11-29 21:04:12 +0800

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,15 @@
+ // 页面只有一个视频可以播放
+ var videos = document.getElementsByTagName('video'); 
+ for (var i = videos.length - 1; i >= 0; i--) {
+     (function(){
+         var p = i;
+         videos[p].addEventListener('play',function(){
+             pauseAll(p);
+         })
+     })()
+ }
+ function pauseAll(index){
+     for (var j = videos.length - 1; j >= 0; j--) {
+         if (j!=index) videos[j].pause();
+     }
+ }
